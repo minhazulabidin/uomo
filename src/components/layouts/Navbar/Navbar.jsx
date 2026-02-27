b634d12bd787d20f867339f6cad49f8f082ec676
introtoxicXXl <minhajulabedin648@gmail.com>
2026-02-28 01:36:11 +0600

    navbar done

--- src/components/layouts/Navbar/Navbar.jsx
+++ src/components/layouts/Navbar/Navbar.jsx
@@ -9,12 +9,19 @@ export default function Navbar() {
     <header>
       <nav>
         <Container>
-          <div className="flex items-center gap-[56px]">
+          <div className="flex items-center gap-14 py-7 justify-between">
             <Logo />
-            <ul className='flex gap-11.25'>
+            <ul className='flex gap-11.25 font-jost'>
               {
                 navbarData.navList.map(item => (
-                  <li key={item.id}><Link href={item.url}>{item.title}</Link></li>
+                  <li className='relative' key={item.id}><Link className="text-sm after:content-[''] after:left-0 after:absolute after:-bottom-0.5 after:w-0 after:h-0.5 after:bg-secondary hover:after:w-3/5 after:duration-300" href={item.url}>{item.title}</Link></li>
+                ))
+              }
+            </ul>
+            <ul className='flex gap-8'>
+              {
+                navbarData.icons.map((icon, index) => (
+                  <li className='cursor-pointer hover:opacity-75' key={index}>{icon}</li>
                 ))
               }
             </ul>
