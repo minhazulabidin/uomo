9e05ddbc33090e00cdb71b52b87fab2cf8ae88f2
introtoxicXXl <minhajulabedin648@gmail.com>
2026-02-27 14:53:57 +0600

    project set up

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Uomo Nextjs Ecommerce App",
+  description: "A modern and stylish ecommerce application built with Next.js, offering a seamless shopping experience for users. Explore a wide range of products, enjoy fast performance, and experience the future of online shopping with Uomo.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="bg-primary text-white text-2xl">Home</div>
+  )
+}
